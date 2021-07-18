d8939583b92228ea36fb9d1e2b88e102f6ecdba4
Ichi <ichi-perm@yandex.ru>
2021-07-18 21:53:04 +0500

    init

---
+++ app/src/main/java/com/example/netologyvoiceassistant/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.netologyvoiceassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.SimpleAdapter
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "MainActivity"
+
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    lateinit var processBar: ProgressBar
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "title2title1")
+            put("Content", "title2title1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "title3title1")
+            put("Content", "title3title1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "title4title1")
+            put("Content", "title4title1")
+        }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        requestInput = findViewById(R.id.text_input_edit)
+
+        val podsList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podsList.adapter = podsAdapter
+
+        val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
+        voiceInputButton.setOnClickListener {
+            Log.d(TAG, "FAB")
+        }
+
+        processBar = findViewById(R.id.progress_bar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
